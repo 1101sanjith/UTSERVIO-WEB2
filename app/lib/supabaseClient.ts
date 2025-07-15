aa6185a8489010e4fc3bdf2b76fc255b2bec522d
1101sanjith <143749984+1101sanjith@users.noreply.github.com>
2025-07-15 20:42:18 +0530

    Update supabaseClient.ts

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -1,12 +1,1 @@
-'use client';
-
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-
-export const supabase = createClientComponentClient({
-  cookieOptions: {
-    name: 'sb',
-    path: '/',
-    sameSite: 'lax',
-    secure: process.env.NODE_ENV === 'production',
-  },
-});
+export const supabase = createClientComponentClient();
