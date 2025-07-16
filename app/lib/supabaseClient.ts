d724a6a482bee031604359531463b8620c864890
1101sanjith <143749984+1101sanjith@users.noreply.github.com>
2025-07-16 13:22:28 +0530

    Update supabaseClient.ts

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -2,4 +2,11 @@
 
 import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 
-export const supabase = createPagesBrowserClient();
+export const supabase = createPagesBrowserClient({
+  cookieOptions: {
+    name: 'sb',
+    path: '/',
+    sameSite: 'Strict', // or 'None' if using HTTPS
+    secure: true,       // always true in production
+  },
+});
